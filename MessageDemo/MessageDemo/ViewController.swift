754f789d93a1106b98526f6e2f5627e0f02762ff
Apple <mm1073856565@163.com>
2016-10-18 18:41:23 +0800

    info

---
+++ MessageDemo/MessageDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MessageDemo
+//
+//  Created by Apple on 16/10/18.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
